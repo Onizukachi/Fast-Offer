a9ffe6dd1e975bd95dd10bf5acad0b56c704f670
Alexei <dark_sao@mail.ru>
2024-08-09 10:02:14 +0300

    fix best configuration

--- fast_offer_front/src/containers/HomePage/queries.js
+++ fast_offer_front/src/containers/HomePage/queries.js
@@ -2,9 +2,6 @@ import api from "@utils/jwtInterceptor.js";
 import { API_BACK_BASE_URL } from "@constants/api.js";
 
 export const positionsQuery = async () => {
-  // почему здесь запрос на api протестируй локально измени только у нжинс маппинг  в конфиге
-  console.log("Я СЕЙЧАС ВЫЗОВУСЬ")
-  console.log(API_BACK_BASE_URL)
   const { data } = await api.get(`${API_BACK_BASE_URL}/api/v1/positions`, {
     headers: {
       'Accept': 'application/json'
